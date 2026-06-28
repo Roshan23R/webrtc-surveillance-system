464aa857ac9e55ba089e63449126d5a2c8d26ee7
Rakesh Roushan <rakeshroushan2341@gmail.com>
2026-06-29 00:17:23 +0530

    add CameraManager class for managing camera operations

---
+++ worker/camera/camera_manager.py
@@ -0,0 +1,62 @@
+from worker.camera.camera_processor import CameraProcessor
+
+
+class CameraManager:
+
+    def __init__(self):
+        self.running_cameras = {}
+
+
+    def start_camera(
+        self,
+        camera_id,
+        rtsp_url
+    ):
+
+        if camera_id in self.running_cameras:
+            return False
+
+        processor = CameraProcessor(
+            camera_id,
+            rtsp_url
+        )
+
+        processor.start()
+
+        self.running_cameras[
+            camera_id
+        ] = processor
+
+        return True
+
+
+    def stop_camera(
+        self,
+        camera_id
+
+    ):
+
+        if camera_id not in self.running_cameras:
+            return False
+
+
+        processor = self.running_cameras[
+            camera_id
+        ]
+
+        processor.stop()
+
+        del self.running_cameras[
+            camera_id
+        ]
+
+        return True
+
+
+    def get_running_cameras(
+        self
+    ):
+
+        return list(
+            self.running_cameras.keys()
+        )
